a7a32e030f5cf3c417d30f545427c3722f349d41
chaney <chaneychan@126.com>
2017-02-16 11:52:28 +0800

    add:update teacherPersonal page course

--- js/createCourse.js
+++ js/createCourse.js
@@ -1,48 +1,4 @@
 $(function () {
-
-    /*发布新课程功能*/
-    // var name = $('#className').val();  //课程名称
-    // var subject =$('#classSubject').val();    //课程科目
-    // var grade =$('#classGrade').val();  //课程年级
-    // var descript =$('#classDec').val();  //课程描述
-    // var groupNumber = $('#discuss').val(); //讨论群号
-    // var price = $('#price').val();  //课程价格
-    // var teacherId = 4;  //教师id
-
-    // var name = $('#className');  //课程名称
-    // var subject =$('#classSubject');    //课程科目
-    // var grade =$('#classGrade');  //课程年级
-    // var descript =$('#classDec');  //课程描述
-    // var groupNumber = $('#discuss'); //讨论群号
-    // var price = $('#price');  //课程价格
-    // var teacherId = 4;  //教师id
-    //
-    // $('#register').click(function () {
-    //     $.ajax({
-    //         url:"http://182.92.220.222:8080/course/create",
-    //         type:"POST",
-    //         // dataType:"jsonp",
-    //         data:{
-    //             name:name.val(),
-    //             subject:subject.val(),
-    //             grade:grade.val(),
-    //             descript:descript.val(),
-    //             groupNumber:groupNumber.val(),
-    //             price:price.val(),
-    //             teacherId:teacherId
-    //         },
-    //         // timeout:1000,
-    //         success:function(str){
-    //             alert(str);
-    //
-    //         },
-    //         error:function () {
-    //
-    //         }
-    //     })
-    // })
-
-
     var name = $('#className');  //课程名称
     var subject = $('#classSubject');    //课程科目
     var grade = $('#classGrade');  //课程年级
@@ -51,23 +7,17 @@ $(function () {
     var price = $('#price');  //课程价格
     var teacherId = 4;  //教师id
 
+    var mask = $('.mask');
     var bar = $('.bar');
     var percent = $('.percent');
-//绑定了`submit`事件。
-    /*$('#createForm').on('submit', (function (e) {
-        e.preventDefault();
-        // var formData = new FormData(this);
-        $(this).ajaxSubmit();
-    }));
 
-
-
-    $("#register").click(function () {
-        // e.preventDefault();
-        $(this).parent().submit();
-    })*/
-
+    $('#register').click(function () {
+        mask.css({
+            display:'block'
+        });
+    })
     $("#createForm").submit(function(){
+
         $(this).ajaxSubmit({
             type: 'POST',
             url: 'http://182.92.220.222:8080/course/create',
@@ -86,12 +36,15 @@ $(function () {
             // resetForm:true,
             beforeSubmit: function () {
                 //上传之前的处理
+
             },
             uploadProgress: function (event, position, total, percentComplete) {
                 //在这里控制进度条
                 //position 已上传了多少
                 //total 总大小
                 //已上传的百分数
+
+
                 var percentVal = percentComplete + '%';
                 bar.width(percentVal);
                 percent.html(percentVal);
--- js/teacherPersonal.js
+++ js/teacherPersonal.js
@@ -2,19 +2,35 @@ $(function () {
     $.ajax({
         url:"http://182.92.220.222:8080/course/readTeacherAll",
         type:"GET",
-        dataType:"jsonp",
         data:{
             page:"1",
             teacherId:"4"
         },
-        // timeout:1000,
-        success:function(str){
-            console.log(str);
+        timeout:1000,
+        //成功回调
+        success:function (str) {
+            console.log(typeof str);
+            var dataArr = str.content;
+            console.log(dataArr);
+            //遍历数组
+            for(var i = 0; i < dataArr.length; i++){
+                var aElement =
+                    '<a href="#" class="courseBox">'+
+                    '<img src="'+ dataArr[i]['cover']+'" alt="" >'+
+                    '<p>课程名称：'+ dataArr[i]['name'] +'</p>'+
+                    '<span class="grade">' + dataArr[i]['subject'] + '</span>'+
+                    '<span class="isFree">¥：'+ dataArr[i]['price']+'</span>'+
+                    '</a>';
+                $('.con_course').append(aElement);
+
+            }
         },
         error:function () {
 
         }
     })
+
+
 });
 
 
