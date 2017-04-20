a45012fd5b65a094b55b34d9d3859cda1d5400b3
chaneychan <chaneychan@126.com>
2017-04-20 11:11:34 +0800

    test bug

--- build/js/login.js
+++ build/js/login.js
@@ -32,16 +32,16 @@ $(function () {
                 'usertype': userType
             },
             success: function (str) {
-                console.log(typeof str);
-
-                var str = JSON.parse(str);
-                console.log(str);
-
-                if (str == error) {
+                if (str == "error") {
                     var infoEle = '<p class="info">账号或者密码错误</p>';
                     $('.info').remove();
                     $('.box_right').append(infoEle);
-                } else if (str.usertype == 'student') {
+                } else {
+                    var str = JSON.parse(str);
+                    console.log(str);
+                }
+
+                if (str.usertype == 'student') {
                     window.location.href = '../view/home.html';
                 } else if (str.usertype == 'teacher') {
                     window.location.href = '../view/teacherPersonal.html';
