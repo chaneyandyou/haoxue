65da92b93f31ed9e0084a9d0cfefb81c32f640e8
chaneychan <chaneychan@126.com>
2017-04-22 18:01:22 +0800

    test bug

--- build/js/home.js
+++ build/js/home.js
@@ -12,8 +12,6 @@ $(function () {
             url: 'http://182.92.220.222:8080/index/hotTeacher',
             type: "GET",
             success: function (dataArr) {
-                console.log(dataArr);
-                console.log(typeof dataArr);
                 for(var index in dataArr){
                     switch (dataArr[index].level)
                     {
@@ -39,33 +37,6 @@ $(function () {
                         '</li>';
                     $(".hotTeacList").append(teacLiEle);
                 }
-                /*for (var i = 0; i < dataArr; i++) {
-                    switch (dataArr[i].level)
-                    {
-                        case 0:
-                            var level = "小学老师";
-                            break;
-                        case 1:
-                            var level = "初中老师";
-                            break;
-                        case 2:
-                            var level = "高中老师";
-                            break;
-                    }
-                    var teacLiEle =
-                        '<li>' +
-                        '<a href="#">' +
-                        '<img src='+ dataArr[i].icon+' alt="讲师">' +
-                        '<h3 class="lec_type">' + level + '</h3>' +
-                        '<span class="lec_name">' + dataArr[i].name + '</span><br>' +
-                        '<span class="lec_school">' + dataArr[i].graduateSchool + '毕业</span>' +
-                        '<p class="lec_detail">' + dataArr[i].profile + '</p>' +
-                        '</a>' +
-                        '</li>';
-                    $(".hotTeacList").append(teacLiEle);
-
-
-                }*/
             },
             error: function (e) {
                 alert("错误！！");
@@ -77,11 +48,22 @@ $(function () {
         });
     }
 
+    /*
+    * 2.搜索按钮
+    * */
+    function searchClick() {
+
+        $(".searchBtn").click(function () {
+            var keyword = $(".searchInp").val();
+            window.location.href = "../view/searchResult.html?keyword=" + keyword;
+        });
+    }
 
     /*
      * 函数执行
      * */
     $.loginStatus(userInfo);
+    searchClick();
     hotTeac();
     $.logout();
 
---
+++ build/js/searchResult.js
@@ -0,0 +1,59 @@
+$(function () {
+    /*
+     * 公共变量
+     * */
+    var userInfo = store.get('userInfo');
+    var keyword = $.getUrlParam("keyword");
+    var grade = $.getUrlParam("grade");
+    var subject = $.getUrlParam("subject");
+
+    function courseRender() {
+        $.ajax({
+            url: 'http://182.92.220.222:8080/course/readSearch',
+            type: "GET",
+            data: {
+                page: 1,
+                keyword: keyword,
+                grade: grade,
+                subject: subject
+            },
+            success: function (str) {
+                console.log(str + '------' + typeof str);
+                var data = str.content;
+                for(var index in data){
+                    var searCourseEle =
+                        '<li>' +
+                        '<dl>' +
+                        '<dt>' +
+                        '<img src=' + data[index].icon + ' >'+
+                        '</dt>' +
+                        '<dd>' +
+                        '<h3>课程名称:' + data[index].name + '</h3>' +
+                        '<p>课程年级:' + data[index].grade + '</p>' +
+                        '<p>课程简介:' + data[index].descript + '</p>' +
+                        '</dd>' +
+                        '</dl>' +
+                        '</li>';
+                    $('.courseUl').append(searCourseEle);
+                }
+
+
+            },
+            error: function (e) {
+                alert("错误！！");
+
+            },
+            xhrFields: {
+                withCredentials: true
+            }
+        });
+    }
+
+
+    /*
+     * 函数执行
+     * */
+    $.loginStatus(userInfo);
+    courseRender();
+
+});
