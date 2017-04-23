f78e729a719523ebb479ee8c78b0f0cb1551f9e6
chaneychan <chaneychan@126.com>
2017-04-23 13:00:23 +0800

    test bug

--- build/js/createChapter.js
+++ build/js/createChapter.js
@@ -3,46 +3,46 @@ $(function () {
     var userInfo = store.get("userInfo");
 
     /*
-    * 通过url获取带过来的courseId参数
-    * */
+     * 通过url获取带过来的courseId参数
+     * */
     var courseId = $.getUrlParam('courseId');
 
     /*
-    * 1.增加新章节按钮点击事件
-    * */
+     * 1.增加新章节按钮点击事件
+     * */
     function addNewChapter() {
         $('.addNew').click(function () {
             $('.con_rig_con').fadeOut(100);
             var createEle =
-                '<div class="con_rig_con clearfix">'+
-                '<p><span>发布新章节</span>严禁发布违反国家法律法规的视频内容</p>'+
-                '<div class="con_left_dec">'+
-                '<label for="chapterName">章节名称</label>'+
-                '<label for="chapterFile">章节视频</label>'+
-                '<label for="chapterDec">章节视频</label>'+
-                '</div>'+
-                '<div class="con_right_inp">'+
-                '<form id="chapterForm" enctype="multipart/form-data">'+
-                '<input type="text" class="myInput" id="chapterName" placeholder="请输入章节名称" name="name">'+
-                '<input type="file" id="chapterFile" name="file">'+
-                '<textarea id="chapterDec" class="myInput" rows="10" autofocus="autofocus" name="descript"></textarea>'+
-                '<input type="submit" value="发布" id="registerChapter">'+
-                '</form>'+
-                '<div class="progress">'+
-                '<div class="bar"></div >'+
-                '<div class="percent">0%</div >'+
-                '</div>'+
-                '</div>'+
+                '<div class="con_rig_con clearfix">' +
+                '<p><span>发布新章节</span>严禁发布违反国家法律法规的视频内容</p>' +
+                '<div class="con_left_dec">' +
+                '<label for="chapterName">章节名称</label>' +
+                '<label for="chapterFile">章节视频</label>' +
+                '<label for="chapterDec">章节视频</label>' +
+                '</div>' +
+                '<div class="con_right_inp">' +
+                '<form id="chapterForm" enctype="multipart/form-data">' +
+                '<input type="text" class="myInput" id="chapterName" placeholder="请输入章节名称" name="name">' +
+                '<input type="file" id="chapterFile" name="file">' +
+                '<textarea id="chapterDec" class="myInput" rows="10" autofocus="autofocus" name="descript"></textarea>' +
+                '<input type="submit" value="发布" id="registerChapter">' +
+                '</form>' +
+                '<div class="progress">' +
+                '<div class="bar"></div >' +
+                '<div class="percent">0%</div >' +
+                '</div>' +
+                '</div>' +
                 '</div>';
             $('.con_right').append(createEle);
         });
     }
 
     /*
-    * 2.通过事件委托，监听发布新章节按钮
-    * */
+     * 2.通过事件委托，监听发布新章节按钮
+     * */
     function newChaterClick() {
-        $('.con_right').on('click','#registerChapter',function () {
+        $('.con_right').on('click', '#registerChapter', function () {
             var bar = $('.bar');
             var percent = $('.percent');
 
@@ -62,31 +62,31 @@ $(function () {
 
                     alert(data);
                     // if(data == "success"){
-                    //     window.location.href = "../view/teacherPersonal.html"
+                    window.location.href = "../view/teacherPersonal.html";
                     // }
                 },
                 error: function (e) {
                     alert("错误！！");
-
+                    window.location.href = "../view/teacherPersonal.html";
                 }
             });
         });
     }
 
     /*
-    * 3.通过课程id查询所有章节,
-    * */
+     * 3.通过课程id查询所有章节,
+     * */
     function chapterInfo() {
         $.ajax({
-            url:'http://182.92.220.222:8080/coursedetails/readCourseAll/'+courseId,
-            success:function (str) {
+            url: 'http://182.92.220.222:8080/coursedetails/readCourseAll/' + courseId,
+            success: function (str) {
                 var chaptersArr = str || [];
                 var nullEle =
                     '<li>暂无章节视频 <button class="del">删除</button></li>';
-                if(chaptersArr.length == 0){
+                if (chaptersArr.length == 0) {
                     $('.chapters').append(nullEle);
                 }
-                for(var i = 0; i < chaptersArr.length; i++){
+                for (var i = 0; i < chaptersArr.length; i++) {
                     var chaptersEle =
                         '<li>' + chaptersArr[i].name + '<button class="del">删除</button></li>';
                     $('.chapters').append(chaptersEle);
@@ -94,7 +94,6 @@ $(function () {
             }
         });
     }
-
 
 
     /*
